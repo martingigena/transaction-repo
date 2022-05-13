d8637fe720d46a67f749ae6cab45befbf0cedd64
Martin <mgigena.edu@gmail.com>
2022-05-13 17:54:47 -0300

    Hig level desing - Forgotten Service Implementation -.-

---
+++ src/main/java/com/mgigena/transactions/services/TransactionServiceImp.java
@@ -0,0 +1,31 @@
+package com.mgigena.transactions.services;
+
+import com.mgigena.transactions.entities.Transaction;
+import com.mgigena.transactions.repository.ITransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+//@Service
+public class TransactionServiceImp implements ITransactionService {
+/*
+    @Autowired
+    private ITransactionRepository transactionRepository;
+
+    @Override
+    public Optional<Transaction> findById(Long id) {
+        return transactionRepository.findById(id);
+    }
+
+    @Override
+    public Transaction save(Transaction transaction) {
+       return transactionRepository.save(transaction);
+    }
+    */
+
+}
