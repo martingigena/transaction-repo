ac35729ac41b4fadb6ae7622440b4c3f670a5db7
Martin <mgigena.edu@gmail.com>
2022-05-13 21:48:39 -0300

    Reviewing I realized that I missed @Repository annotation, I know I'm not working with DB but it can’t hurt to add it

--- src/main/java/com/mgigena/transactions/repository/ITransactionRepository.java
+++ src/main/java/com/mgigena/transactions/repository/ITransactionRepository.java
@@ -2,6 +2,8 @@ package com.mgigena.transactions.repository;
 
 import com.mgigena.transactions.entities.Transaction;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+//@Repository
 public interface ITransactionRepository /*extends CrudRepository<Transaction, Long> */{
 }
